f848a8624c0738157be2f47dd5fb83f7c42bd4e2
DevilYuri <Angelokn2006@gmail.com>
2025-05-29 00:55:06 +0300

    site

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('.nav-menu').classList.toggle('active');
-});
-function createPetals() {
-    const petalCount = window.innerWidth < 768 ? 20 : 50;
-    // остальной код
-}
